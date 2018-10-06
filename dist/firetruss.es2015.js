233513530580660f56443bf6ddc7b0847aba2ecd
Piotr Kaminski <piotr@ideanest.com>
2018-10-06 00:24:27 -0700

    release 0.9.5

--- dist/firetruss.es2015.js
+++ dist/firetruss.es2015.js
@@ -3377,7 +3377,7 @@ let bridge;
 let logging;
 const workerFunctions = {};
 // This version is filled in by the build, don't reformat the line.
-const VERSION = 'dev';
+const VERSION = '0.9.5';
 
 
 class Truss {
--- dist/firetruss.umd.js
+++ dist/firetruss.umd.js
@@ -3732,7 +3732,7 @@
 	var logging;
 	var workerFunctions = {};
 	// This version is filled in by the build, don't reformat the line.
-	var VERSION = 'dev';
+	var VERSION = '0.9.5';
 
 
 	var Truss = function Truss(rootUrl) {
