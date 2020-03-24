1ecc4c45239ccd6a478ca46db7c6eb668acbf8cd
Piotr Kaminski <piotr@ideanest.com>
2020-03-23 22:59:40 -0700

    release 3.0.5

--- dist/firetruss.es2015.js
+++ dist/firetruss.es2015.js
@@ -3344,7 +3344,7 @@ let bridge;
 let logging;
 const workerFunctions = {};
 // This version is filled in by the build, don't reformat the line.
-const VERSION = 'dev';
+const VERSION = '3.0.5';
 
 
 class Truss {
