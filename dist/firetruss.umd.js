9dcf5d5e577e901b6db901a781a5cd7fa1b5d005
Piotr Kaminski <piotr@ideanest.com>
2022-07-26 20:03:48 -0700

    release 4.1.5

--- dist/firetruss.umd.js
+++ dist/firetruss.umd.js
@@ -3699,7 +3699,7 @@
   var bridge, logging;
   var workerFunctions = {};
   // This version is filled in by the build, don't reformat the line.
-  var VERSION = 'dev';
+  var VERSION = '4.1.5';
 
 
   var Truss = function Truss(rootUrl) {
