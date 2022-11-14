6bf30fbd503eead881df907ce536d26ea947e61b
Piotr Kaminski <piotr@ideanest.com>
2022-11-13 22:28:52 -0800

    release 5.1.0

--- dist/firetruss.es2015.js
+++ dist/firetruss.es2015.js
@@ -3650,7 +3650,7 @@ function toFirebaseJson(object) {
 var bridge, logging;
 var workerFunctions = {};
 // This version is filled in by the build, don't reformat the line.
-var VERSION = 'dev';
+var VERSION = '5.1.0';
 
 
 var Truss = function Truss(rootUrl) {
