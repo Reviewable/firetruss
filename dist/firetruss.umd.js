8d5cea0207feb73e38c2b9d2d344728d0cd024e8
Piotr Kaminski <piotr@ideanest.com>
2025-11-18 14:20:42 -0800

    release 7.5.3

--- dist/firetruss.umd.js
+++ dist/firetruss.umd.js
@@ -3517,7 +3517,7 @@
   let bridge, logging;
   const workerFunctions = {};
   // This version is filled in by the build, don't reformat the line.
-  const VERSION = 'dev';
+  const VERSION = '7.5.3';
 
 
   class Truss {
